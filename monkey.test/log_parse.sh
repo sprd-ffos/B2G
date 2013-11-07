77fb0c091c24654de28449a848efcedc6a594ca4
lianxiang.zhou <lianxiang.zhou@spreadtrum.com>
2013-11-07 14:07:20 +0800

    bug#237065 add more general key words when filter log, and ignore case
    [bug number ] 237065
    [root cause ]
    [changes ]
    [side effects]
    [reviewers ]

--- monkey.test/log_parse.sh
+++ monkey.test/log_parse.sh
@@ -19,7 +19,7 @@ do
     if [ -f ${log}/$file ]
     then
         echo "[kmsg: $file]" >> $parse_file
-        grep -f filter.kmsg ${log}/$file >> $parse_file
+        grep -i -f filter.kmsg ${log}/$file >> $parse_file
         echo >> $parse_file
     fi
 done
@@ -31,7 +31,7 @@ do
     if [ -f ${log}/$file ]
     then
         echo "[logcat main: $file]" >> $parse_file
-        grep -f filter.kmsg ${log}/$file >> $parse_file
+        grep -i -f filter.kmsg ${log}/$file >> $parse_file
         echo >> $parse_file
     fi
 done
